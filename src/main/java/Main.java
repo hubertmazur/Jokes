adc8a5fa9ae82a2f208f3c35f9bbbfe9929f5f3d
hubert <hubert.mazur@vp.pl>
2019-07-01 18:03:10 +0200

    Adding main method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         APIJokeSource apiJokeSource = new APIJokeSource();
-
-        apiJokeSource.getSource(5).forEach(joke -> System.out.println(joke.getValue()));
-
-
+        System.out.println("===Program wyświetla żarty o Chucku Norrisie===");
+        int validaton = 0;
+        while (validaton < 5) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Wprowadz ilość żartów do wyświetlenia :");
+                int how = scanner.nextInt();
+                apiJokeSource.getSource(how).forEach(joke -> System.out.println(joke.getValue()));
+            } catch (InputMismatchException e) {
+                System.out.println("Wprowadzono błędne dane.");
+                validaton++;
+            }
+        }
     }
 }
